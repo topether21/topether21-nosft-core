0ac0a5d3272a086ecc89736e9e3b3b66be0870bb
RuGe <70239531+ruge0326@users.noreply.github.com>
2023-03-30 11:24:59 -0600

    0.0.9

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
-import Nosft, { get as nosftGet } from './src/services/nosft';
-import { NostrRelay } from './src/services/relay';
+// import Nosft, { get as nosftGet } from './src/services/nosft';
 
-export function configure({ network, nosftBaseUrl }: { network: 'testnet' | 'mainnet'; nosftBaseUrl?: string }) {
-    const nosftService: Nosft = nosftGet({ baseUrl: nosftBaseUrl, network });
-    return {
-        nosft: nosftService,
-    };
-}
+// export function configure({ network, nosftBaseUrl }: { network: 'testnet' | 'mainnet'; nosftBaseUrl?: string }) {
+//     const nosftService: Nosft = nosftGet({ baseUrl: nosftBaseUrl, network });
+//     return {
+//         nosft: nosftService,
+//     };
+// }
 
-const nosft: Nosft = nosftGet();
-export { nosft };
-export { NostrRelay };
+// const nosft: Nosft = nosftGet();
+// export { nosft };
+
+export { NostrRelay } from './src/services/relay';
 export * from './src/types/relay';
